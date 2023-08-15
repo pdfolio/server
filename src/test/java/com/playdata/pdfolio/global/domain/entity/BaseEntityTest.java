8c25c800c81436398fc9868f664bb58a6acfa93e
ktk8916 <ktk8916@naver.com>
2023-08-15 22:15:59 +0900

    컬럼 삭제 테스트

---
+++ src/test/java/com/playdata/pdfolio/global/domain/entity/BaseEntityTest.java
@@ -0,0 +1,31 @@
+package com.playdata.pdfolio.global.domain.entity;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class BaseEntityTest {
+
+    @Test
+    void 컬럼_삭제_성공() {
+        //given
+        BaseEntity baseEntity = new BaseEntity() {};
+        //when
+        baseEntity.deleteColumn();
+        //then
+        assertThat(baseEntity.getIsDeleted()).isTrue();
+        assertThat(baseEntity.getDeletedAt()).isNotNull();
+    }
+
+    @Test
+    void 삭제된_컬럼_삭제시_예외() {
+        //given
+        BaseEntity baseEntity = new BaseEntity() {};
+        baseEntity.deleteColumn();
+        //when
+        assertThrows(RuntimeException.class, baseEntity::deleteColumn, "이미 삭제된 컬럼");
+        //then
+    }
+}
