6a3d306f57cbe9b6f50680057cbd9aa1c0b58049
ktk8916 <ktk8916@naver.com>
2023-08-16 00:18:53 +0900

    이름변경 Common -> Custom

--- src/main/java/com/playdata/pdfolio/global/exception/CustomRestControllerAdvice.java
+++ src/main/java/com/playdata/pdfolio/global/exception/CustomRestControllerAdvice.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class CommonRestControllerAdvice {
+public class CustomRestControllerAdvice {
 
     @ExceptionHandler(CommonException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
