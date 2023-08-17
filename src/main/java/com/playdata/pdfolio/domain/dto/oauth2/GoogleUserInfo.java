565842e52772e54afcd136e8dd34c48597c3c7aa
ktk8916 <ktk8916@naver.com>
2023-08-17 20:48:42 +0900

    oauth user info 추가

---
+++ src/main/java/com/playdata/pdfolio/domain/dto/oauth2/GoogleUserInfo.java
@@ -0,0 +1,4 @@
+package com.playdata.pdfolio.domain.dto.oauth2;
+
+public class GoogleUserInfo{
+}
---
+++ src/main/java/com/playdata/pdfolio/domain/dto/oauth2/Oauth2UserInfo.java
@@ -0,0 +1,6 @@
+package com.playdata.pdfolio.domain.dto.oauth2;
+
+public interface Oauth2UserInfo {
+
+
+}
