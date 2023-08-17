b96bf6c9b27b3c238540c9ac8a3314ae9d93f946
ktk8916 <ktk8916@naver.com>
2023-08-17 20:46:20 +0900

    지원하지 않는 oauth2 예외 추가

--- src/main/java/com/playdata/pdfolio/global/exception/ExceptionType.java
+++ src/main/java/com/playdata/pdfolio/global/exception/ExceptionType.java
@@ -1,5 +1,6 @@
 package com.playdata.pdfolio.global.exception;
 
+import com.playdata.pdfolio.oauth2.exception.NotSupportedOauth2Exception;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
@@ -11,9 +12,9 @@ import java.util.Objects;
 @AllArgsConstructor
 public enum ExceptionType {
 
-    UNKNOWN_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "알수없는 에러"),
-    COLUMN_ALREADY_DELETED(HttpStatus.BAD_REQUEST, "이미 삭제된 컬럼입니다.", ColumnAlreadyDeletedException.class);
-
+    UNKNOWN_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 서버 에러"),
+    COLUMN_ALREADY_DELETED(HttpStatus.BAD_REQUEST, "이미 삭제된 컬럼입니다.", ColumnAlreadyDeletedException.class),
+    NOT_SUPPORTED_OAUTH2(HttpStatus.BAD_REQUEST, "지원하지 않는 Oauth2 입니다.", NotSupportedOauth2Exception.class);
     private final HttpStatus httpStatus;
     private final String message;
     private Class<? extends PdFolioException> type;
