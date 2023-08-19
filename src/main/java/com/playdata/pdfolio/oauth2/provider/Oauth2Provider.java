2adb3f0dba312f90e66f05eac805049988ebe6c3
ktk8916 <ktk8916@naver.com>
2023-08-20 01:24:21 +0900

    name 필드 추가

--- src/main/java/com/playdata/pdfolio/oauth2/provider/Oauth2Provider.java
+++ src/main/java/com/playdata/pdfolio/oauth2/provider/Oauth2Provider.java
@@ -1,6 +1,7 @@
 package com.playdata.pdfolio.oauth2.provider;
 
 public interface Oauth2Provider {
+    String getName();
     String getClientId();
     String getClientSecret();
     String getRedirectUri();
