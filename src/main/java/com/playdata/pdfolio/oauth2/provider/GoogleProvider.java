0778fa73879e61b53f13d5d123e5e13d796e9793
ktk8916 <ktk8916@naver.com>
2023-08-19 23:53:22 +0900

    프로퍼티 변경

--- src/main/java/com/playdata/pdfolio/oauth2/provider/GoogleProvider.java
+++ src/main/java/com/playdata/pdfolio/oauth2/provider/GoogleProvider.java
@@ -10,14 +10,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class GoogleProvider implements Oauth2Provider {
 
-    @Value("${oauth2.user.github.client-id}")
+    @Value("${oauth2.user.google.client-id}")
     private String clientId;
-    @Value("${oauth2.user.github.client-secret}")
+    @Value("${oauth2.user.google.client-secret}")
     private String clientSecret;
-    @Value("${oauth2.user.github.redirect-uri}")
+    @Value("${oauth2.user.google.redirect-uri}")
     private String redirectUri;
-    @Value("${oauth2.user.github.access-token-uri}")
+    @Value("${oauth2.user.google.access-token-uri}")
     private String accessTokenUri;
-    @Value("${oauth2.user.github.user-info-uri}")
+    @Value("${oauth2.user.google.user-info-uri}")
     private String userInfoUri;
 }
