36f7ae20e805c9fb1836aa96de5ae0631bfba05d
ktk8916 <ktk8916@naver.com>
2023-08-21 01:51:25 +0900

    쿠키 추가

--- src/main/java/com/playdata/pdfolio/oauth2/controller/Oauth2Controller.java
+++ src/main/java/com/playdata/pdfolio/oauth2/controller/Oauth2Controller.java
@@ -1,7 +1,11 @@
 package com.playdata.pdfolio.oauth2.controller;
 
+import com.playdata.pdfolio.domain.response.oauth2.Oauth2LoginResponse;
 import com.playdata.pdfolio.oauth2.service.Oauth2Service;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,10 +16,18 @@ public class Oauth2Controller {
     private final Oauth2Service oauth2Service;
 
     @GetMapping("/{provider}")
-    public void login(
+    public ResponseEntity<Oauth2LoginResponse> login(
             @PathVariable("provider") String provider,
-            @RequestParam("code") String code)
+            @RequestParam("code") String code,
+            HttpServletResponse response)
     {
-        oauth2Service.login(provider, code);
+        Oauth2LoginResponse loginResponse = oauth2Service.login(provider, code);
+        Cookie cookie = new Cookie("refreshToken", loginResponse.jwtToken().refreshToken());
+        cookie.setSecure(true);
+        cookie.setHttpOnly(true);
+
+        response.addCookie(cookie);
+        return ResponseEntity.ok().body(loginResponse);
     }
+
 }
