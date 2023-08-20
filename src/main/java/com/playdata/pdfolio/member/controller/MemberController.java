6b45d5959252a6b26f37bb3d9f0d7a43130600d5
ktk8916 <ktk8916@naver.com>
2023-08-21 01:50:55 +0900

    test..

---
+++ src/main/java/com/playdata/pdfolio/member/controller/MemberController.java
@@ -0,0 +1,17 @@
+package com.playdata.pdfolio.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/member")
+public class MemberController {
+
+    @GetMapping
+    public String test(){
+        return "pass";
+    }
+}
