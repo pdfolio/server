437868f5c5391f575adee3a43df66f74ea28071c
ktk8916 <ktk8916@naver.com>
2023-08-21 01:45:13 +0900

    토큰 예외 추가

--- src/main/java/com/playdata/pdfolio/global/exception/ExceptionType.java
+++ src/main/java/com/playdata/pdfolio/global/exception/ExceptionType.java
@@ -1,5 +1,8 @@
 package com.playdata.pdfolio.global.exception;
 
+import com.playdata.pdfolio.auth.exception.AccessTokenExpiredException;
+import com.playdata.pdfolio.auth.exception.InvalidTokenException;
+import com.playdata.pdfolio.auth.exception.TokenNotFoundException;
 import com.playdata.pdfolio.oauth2.exception.NotSupportedOauth2Exception;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,7 +17,11 @@ public enum ExceptionType {
 
     UNKNOWN_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 서버 에러"),
     COLUMN_ALREADY_DELETED(HttpStatus.BAD_REQUEST, "이미 삭제된 컬럼입니다.", ColumnAlreadyDeletedException.class),
-    NOT_SUPPORTED_OAUTH2(HttpStatus.BAD_REQUEST, "지원하지 않는 Oauth2 입니다.", NotSupportedOauth2Exception.class);
+    NOT_SUPPORTED_OAUTH2(HttpStatus.BAD_REQUEST, "지원하지 않는 Oauth2 입니다.", NotSupportedOauth2Exception.class),
+    TOKEN_NOT_FOUND_EXCEPTION(HttpStatus.UNAUTHORIZED, "요청에서 토큰을 찾을 수 없습니다.", TokenNotFoundException.class),
+    INVALID_TOKEN_EXCEPTION(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다.",InvalidTokenException .class),
+    ACCESS_TOKEN_EXPIRE_EXCEPTION(HttpStatus.FORBIDDEN, "액세스 토큰이 만료되었습니다.", AccessTokenExpiredException.class);
+
     private final HttpStatus httpStatus;
     private final String message;
     private Class<? extends PdFolioException> type;
