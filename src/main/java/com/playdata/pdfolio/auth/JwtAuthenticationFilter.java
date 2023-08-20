ddac9511f42b418717953107999a3cf41c77caee
ktk8916 <ktk8916@naver.com>
2023-08-21 01:48:43 +0900

    토큰 검증 필터 추가

---
+++ src/main/java/com/playdata/pdfolio/auth/JwtAuthenticationFilter.java
@@ -0,0 +1,46 @@
+package com.playdata.pdfolio.auth;
+
+import com.playdata.pdfolio.auth.exception.AccessTokenExpiredException;
+import com.playdata.pdfolio.auth.exception.InvalidTokenException;
+import com.playdata.pdfolio.auth.exception.TokenNotFoundException;
+import com.playdata.pdfolio.jwt.service.JwtService;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String bearerToken = extractToken(request);
+        Authentication authentication = getAuthentication(bearerToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(request, response);
+    }
+
+    private Authentication getAuthentication(String bearerToken) {
+        Map<String, Object> claims = jwtService.getClaims(bearerToken);
+        return new UsernamePasswordAuthenticationToken(claims, "");
+    }
+
+    private String extractToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if(!StringUtils.hasText(bearerToken)){
+            throw new TokenNotFoundException();
+        }
+        return bearerToken;
+    }
+}
