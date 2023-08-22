d4e79f39299601662469017bd6f1998524b29565
isnanaOne <1onlyonehana@gmail.com>
2023-08-22 17:24:58 +0900

    하...

--- src/main/java/com/playdata/pdfolio/gather/GatherRepository.java
+++
@@ -1,8 +0,0 @@
-package com.playdata.pdfolio.gather;
-
-import com.playdata.pdfolio.domain.entity.gather.Gather;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GatherRepository extends JpaRepository<Gather, Long> {
-
-}
